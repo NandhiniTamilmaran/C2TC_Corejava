7691432ef529d0ba1959a8d5761b00295d9c2057
NandhiniTamilmaran <nandhinitamilmaran340@gmail.com>
2025-07-21 18:58:13 +0530

    day2 Programs

---
+++ C2TC_Nandhini/src/day1/day2ifdemo.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class day2ifdemo {
+	public static void main(String[] args) {
+
+		int x = 5;
+		if (x == 5) {
+			
+	
+			//System.out.println("Value of x is not 5");
+		System.out.println("Value of x is 5");
+	}
+
+}
+}
---
+++ C2TC_Nandhini/src/day1/day2ifelse.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class day2ifelse {
+	public static void main(String[] args) {
+		int age=11;
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else {
+			System.out.println("Sorry! You need to wait more");
+		}
+		
+	}
+}
