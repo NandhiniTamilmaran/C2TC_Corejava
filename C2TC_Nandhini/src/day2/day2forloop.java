3732488ec996973d371e309166eb55c071344499
NandhiniTamilmaran <nandhinitamilmaran340@gmail.com>
2025-07-21 19:06:17 +0530

    Day2 programs

---
+++ C2TC_Nandhini/src/day2/day2forloop.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class day2forloop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=100 ; i++)   
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+}
