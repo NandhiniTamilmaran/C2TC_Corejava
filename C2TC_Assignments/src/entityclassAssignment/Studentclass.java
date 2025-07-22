6b230e3bae9e23fc3d42aca662f21e44969d26cf
NandhiniTamilmaran <nandhinitamilmaran340@gmail.com>
2025-07-22 12:13:55 +0530

    assignmets

---
+++ C2TC_Assignments/src/entityclassAssignment/Studentclass.java
@@ -0,0 +1,14 @@
+package entityclassAssignment;
+import java.util.Scanner;
+public class Studentclass {
+	public static void main(String[] args) {
+		
+	
+Scanner  nand=new Scanner(System.in);
+System.out.println("Enter the students name:");
+String name=nand.nextLine();
+System.out.println("Enter the roll no");
+int rollno=nand.nextInt();
+System.out.println("Enter the percentage:");
+}
+}
